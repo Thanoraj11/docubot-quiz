53c0f3824bf1ddf1a4f4efda25ccef252ac9541b
Thanoraj11 <muthulingam.thanoraj@flipick.com>
2023-07-08 11:47:21 +0530

    Chat engine complete

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 import streamlit as st
+from llama_index.llms import OpenAI, ChatMessage
+from typing import List
+from nltk.translate.bleu_score import sentence_bleu
+
+
 from llama_index import (
     GPTVectorStoreIndex, Document, SimpleDirectoryReader,
     QuestionAnswerPrompt, LLMPredictor, ServiceContext
 )
 
 
-from langchain import OpenAI
+# from langchain import OpenAI
 from tempfile import NamedTemporaryFile
 from llama_index import download_loader
 
@@ -22,17 +27,14 @@ from llama_index import Prompt
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-question_rules = "Question Rule 1: Do not ask questions about very small details such as specific numbers.\nQuestion Rule 2: When you run out of questions, let me know this and then quiz me on the questions I've already responded to. Present the questions one at a time in random order."
-answer_rules = "Feedback Rule 1: If my response is correct, you give feedback and then move on to the next question (Step 1 again).\nFeedback Rule 2: If the answer is incorrect or only partly correct, you give feedback that helps me move toward the correct answer. After your feedback, say something in the style of 'try again'.\nFeedback Rule 3: It is very important that you dont give away the correct answer in the feedback to partly correct or incorrect responses."
-context_str = question_rules+answer_rules
-template = ("When I send the message 'start', I want you to be my tutor and teach me about the content by asking me questions about it and providing me with feedback on the answers I give to your questions.\nWe will do this in a loop with the following steps:\n(1) You ask me a question about the text\n(2) I respond to your question\n(3) You give me feedback on my response.\n"
-    "---------------------\n"
-    "{context_str}"
-    "\n---------------------\n"
-    "All information given above, Generate answer for the current query : {query_str}\n"
-    )
 
-QA_TEMPLATE = Prompt(template)
+
+
+llm = OpenAI(temperature=0, model="gpt-3.5-turbo-0613")
+
+
+
+
 
 
 def process_pdf(uploaded_file):
@@ -46,7 +48,7 @@ def process_pdf(uploaded_file):
     
     if "index" not in st.session_state:
         index = GPTVectorStoreIndex.from_documents(documents,service_context=service_context)
-        query_engine = index.as_query_engine(text_qa_template=QA_TEMPLATE)
+        query_engine = index.as_query_engine()
         st.session_state.index = query_engine
     # st.session_state.index = index
     return st.session_state.index
@@ -54,18 +56,63 @@ def process_pdf(uploaded_file):
 
 
 
-uploaded_file = st.file_uploader("Upload a PDF file", type="pdf")
-if uploaded_file is not None:
-
-    if "index" not in st.session_state:
-        st.session_state.index = process_pdf(uploaded_file)
-        st.success("Index created successfully")
 
 
+class TutorAgent:
+    def __init__(self, chat_history: List[ChatMessage] = []):
+        self._llm = llm
+        self._chat_history = chat_history
+        self.score_threshold = 0.7  # adjust this as per your requirements
+        self.expected_answer = ""  # Initialize expected_answer attribute
 
-query = st.text_input("Enter query prompt")
-asl = st.button("Submit")
+    def reset(self):
+        self._chat_history = []
 
-if asl:
-    resp  = st.session_state.index.query(query).response
-    st.write(resp)
+    def extract_keywords(self, text: str) -> List[str]:
+        self.reset()
+        message = self._llm.chat([ChatMessage(role="system", content=f"Please list 10 keywords or topics from the following text: {text}")])
+        keywords = message.message.content.split('\n')  # Assuming the model returns a newline-separated list
+        return keywords
+
+    def generate_question_answer(self, keyword: str) -> (str, str):
+        self.reset()
+        message = self._llm.chat([ChatMessage(role="system", content=f"Generate a question about the topic: {keyword} with the answer separated by a newline.")])
+        question, expected_answer = message.message.content.split('\n')  # Assuming the model returns question and answer separated by a newline
+        # st.write(question)
+        # st.write(expected_answer)
+
+        self.expected_answer = expected_answer
+        return question, expected_answer
+
+    def give_feedback(self, user_answer: str) -> (str, float):
+        self._chat_history.append(ChatMessage(role="user", content=user_answer))
+
+        feedback_instructions = """
+        Please provide detailed feedback based on the following principles:
+       
+        Do not give away the correct answer if the answer is incorrect or only partly correct.
+        Make sure to mention the principle numbers that are relevant to your feedback.
+        If the answer is incorrect or only partly correct, mention the areas to improve.
+
+        Expected answer: {self.expected_answer}
+        User's answer: {answer}
+        """
+        
+        self._chat_history.append(ChatMessage(role="system", content=feedback_instructions))
+
+        message = self._llm.chat(self._chat_history)
+        feedback = message.message.content
+
+        score = self.get_score(user_answer, self.expected_answer)
+        return feedback, score
+
+    def get_score(self, user_answer: str, expected_answer: str) -> float:
+        return sentence_bleu([expected_answer.split()], user_answer.split())
+
+
+
+try:
+
+    tutor = TutorAgent()
+
+    st.title("AI Tutor")

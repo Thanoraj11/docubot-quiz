dfa646af4ea852965a83efacf1d8db5df2434286
Jude Sajith <judesajith.aj@gmail.com>
2023-07-11 14:25:25 +0530

    ssss

--- pages/local.py
+++ pages/local.py
@@ -2,19 +2,13 @@ import streamlit as st
 import openai
 import os
 
-
-
-
-
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-# GPT-3.5 Turbo parameters
 ENGINE = "text-davinci-003"
-MAX_TOKENS = 60
+MAX_TOKENS = 100
 
-keywords = ['Python', 'Java', 'JavaScript']  # Your list of keywords here
+keywords = ['Python', 'Java', 'JavaScript']
 
-# Initialization
 if 'counter' not in st.session_state:
     st.session_state.counter = 0
 if 'score' not in st.session_state:
@@ -38,17 +32,11 @@ def grade_answer(question, user_answer):
     )
     
     output_text = response.choices[0].text.strip()
-    
-    # Here you would split the response and convert string to boolean
-    # Make sure the model is returning the output in the format you are expecting
-    # Assuming the format of output is: "True: The answer is correct."
     bool_value, feedback = output_text.split(':', 1)
     bool_value = bool_value.strip().lower() == 'true'
     feedback = feedback.strip()
-    st.write(bool_value, feedback)
-    
-    return bool_value, feedback
 
+    return bool_value, feedback
 
 def generate_question(keyword):
     prompt = f"Generate a question about the following topic : {keyword}"
@@ -57,7 +45,6 @@ def generate_question(keyword):
 
 st.title("Quizbot Application")
 
-# Start learning session
 if st.button("Start Learning Session"):
     st.session_state.counter = 0
     st.session_state.score = 0
@@ -65,34 +52,34 @@ if st.button("Start Learning Session"):
 
 st.write("Your current score:", st.session_state.score)
 
-if st.session_state.counter < len(keywords):
-    keyword = keywords[st.session_state.counter]
-    question = generate_question(keyword)
-    st.session_state.conversations.append({
-        'keyword': keyword,
-        'question': question,
-        'user_answer': None,
-        'feedback': None,
-    })
-
-if len(st.session_state.conversations) > 0:
-    current_conversation = st.session_state.conversations[-1]
-
+if len(st.session_state.conversations) < len(keywords):
     user_answer = st.text_area("Your answer:")
+
     if user_answer:
-        current_conversation['user_answer'] = user_answer
+        if len(st.session_state.conversations) > 0:
+            current_conversation = st.session_state.conversations[-1]
+            current_conversation['user_answer'] = user_answer
 
-        # Grade the answer
-        correct, feedback = grade_answer(current_conversation['question'], user_answer)
-        st.session_state.score += int(correct)
-        current_conversation['feedback'] = feedback
+            correct, feedback = grade_answer(current_conversation['question'], user_answer)
+            st.session_state.score += int(correct)
+            current_conversation['feedback'] = feedback
 
-        # Move to the next question
-        st.session_state.counter += 1
+            st.session_state.counter += 1
+
+        if st.session_state.counter < len(keywords):
+            keyword = keywords[st.session_state.counter]
+            question = generate_question(keyword)
+
+            st.session_state.conversations.append({
+                'keyword': keyword,
+                'question': question,
+                'user_answer': None,
+                'feedback': None,
+            })
 
 for i, conversation in enumerate(reversed(st.session_state.conversations), start=1):
     with st.expander(f"Thread {len(st.session_state.conversations)-i+1}", expanded=(i==1)):
-        st.write(f"**Question**\n\n", f"\n{conversation['question']}")
-        if not st.session_state.conversations == 0
-            st.write(f"**Your Answer**\n\n", f"\n{conversation['user_answer']}")
-            st.write(f"**Feedback**\n\n", f"\n{conversation['feedback']}")
+        st.write(f"**Question**\n\n{conversation['question']}")
+        if conversation['user_answer'] is not None:
+            st.write(f"**Your Answer**\n\n{conversation['user_answer']}")
+            st.write(f"**Feedback**\n\n{conversation['feedback']}")
